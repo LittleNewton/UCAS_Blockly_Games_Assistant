1414c0644be63b0675ed6af05f75a4ff1e920e06
Liu-Peng@Newton-PC-3 <littleNewton6@gmail.com>
2021-12-14 10:26:22 +0800

    New Feature: Extension 和 content script 的交互已建立

--- client/js/inject.js
+++ client/js/inject.js
@@ -1,7 +1,7 @@
 /*
  * 文件名：inject.js
  * 创建时间：2021 Sept. 23
- * 修改时间：2021 Dec.  07
+ * 修改时间：2021 Dec.  13
  * 作者：刘鹏
  * 文件描述：类 Chrome 浏览器的前端代码
  */
@@ -9,19 +9,16 @@
 
 
 /*
- * Let's get started
+ * 与服务器建立 webSocket 连接
  */
-function hello(){
-    alert('让我们开始编程吧！')
-}
-console.log("让我们开始！ from inject.js")
+let socket = new WebSocket('ws://localhost:9999')
 
 
 
 /*
- * 与服务器建立 webSocket 连接
+ * 与 Extension 建立联系
  */
-let socket = new WebSocket('ws://localhost:9999')
+const extension_id = 'abpilfbfomcllhheghclnlhgkhadlpop'
 
 
 
@@ -42,15 +39,11 @@ function register (username, password) {
         let data = JSON.parse(evt.data)
         if (data.funcCode == '0') {
             console.log("注册失败")
-            alert("注册失败")
         } else if (data.funcCode == '1') {
             console.log("注册成功")
             console.log("当前用户：" + data.current_user)
-            alert("注册成功")
-            alert("当前用户：" + data.current_user)
         } else {
             console.log("服务器无响应")
-            alert("服务器无响应")
         }
     }
 }
@@ -74,13 +67,9 @@ function login (username, password) {
         if (data.funcCode == '0') {
             console.log("登录失败")
             console.log(data.error)
-            alert("登录失败")
-            alert(data.error)
         } else if (data.funcCode == '1') {
             console.log("登录成功")
             console.log("当前用户：" + data.current_user)
-            alert("登录成功")
-            alert("当前用户：" + data.current_user)
         }
     }
 }
@@ -125,12 +114,13 @@ function upload_game_data () {
 
     socket.onmessage = function(evt) {
         let response = JSON.parse(evt.data)
+
         if (response.funcCode == 0) {
-            alert("发送本地游戏数据失败")
+            console.log("发送本地游戏数据失败")
         } else if (response.funcCode == 1) {
-            alert("发送本地游戏数据成功")
+            console.log("发送本地游戏数据成功")
         } else {
-            alert("因为其它原因发送失败")
+            console.log("因为其它原因发送失败")
         }
     }
 }
@@ -145,7 +135,7 @@ function download_game_data () {
 
     socket.send(JSON.stringify(data))
     // TO BE DELETED check 输出
-    alert('拉取请求已提交')
+    console.log('拉取请求已提交')
 
     // 收取服务器的反馈
     // (1) 如果服务器返回的 funcCode == 0，即错误返回，就发一个警告
@@ -154,21 +144,20 @@ function download_game_data () {
     socket.onmessage = function (evt) {
         let response = JSON.parse(evt.data)
         if (response.funcCode == 0) {
-            alert.log("CLIENT: 请求拉取游戏进度失败，云端没有存档")
+            console.log("CLIENT: 请求拉取游戏进度失败，云端没有存档")
         } else if (response.funcCode == '1') {
-            alert("CLIENT: 请求拉取云端游戏数据成功")
-
+            console.log("CLIENT: 请求拉取云端游戏数据成功")
             let len = response.games.length
+            
             for (let i = 0; i < len; i++) {
                 window.localStorage.setItem(response.games[i].gameName, response.games[i].xml)
                 console.log(response.games[i].gameName,response.games[i].xml)
             }
-            alert("CLIENT: 游戏存档已成功储存至本地")
+            console.log("CLIENT: 游戏存档已成功储存至本地")
         } else if (response.funcCode == '2') {
-            alert("CLIENT: 用户未登录")
+            console.log("CLIENT: 用户未登录")
         } else {
-            alert("CLIENT: 发生未知错误")
-
+            console.log("CLIENT: 发生未知错误")
         }
     }
 }
@@ -197,7 +186,7 @@ function clear_server_game_content () {
 
 
 
-// 在集成到 HTML 之前，这就是测试模块
-socket.onopen = function () {
-    // login("newton", '123456')
+// Content Script 发送消息到 Background
+function say_hi () {
+    chrome.runtime.sendMessage(extension_id, {'words': "hello", 'name': 'inject.js'})
 }
--- client/js/popup.js
+++ client/js/popup.js
@@ -23,11 +23,6 @@ $('#invoke_background_js').click(e => {
 	bg.testBackground();
 });
 
-// 欢迎
-$('#welcome_js').click(e => {
-	var bg = hello();
-});
-
 // 注册
 $('#register_js').click(e => {
 	var username = prompt('请输入用户名');
@@ -137,14 +132,14 @@ $('#highlight_tab').click(() => {
 	chrome.tabs.highlight({tabs: 0});
 });
 
-// popup主动发消息给content-script
+// popup 主动发消息给 content-script
 $('#send_message_to_content_script').click(() => {
 	sendMessageToContentScript('你好，我是popup！', (response) => {
 		if(response) alert('收到来自content-script的回复：'+response);
 	});
 });
 
-// 监听来自content-script的消息
+// 监听来自 content-script 的消息
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
 	console.log('收到来自content-script的消息：');
@@ -152,7 +147,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 	sendResponse('我是popup，我已收到你的消息：' + JSON.stringify(request));
 });
 
-// popup与content-script建立长连接
+// popup 与 content-script建立长连接
 $('#connect_to_content_script').click(() => {
 	getCurrentTabId((tabId) => {
 		var port = chrome.tabs.connect(tabId, {name: 'test-connect'});
@@ -199,53 +194,3 @@ function sendMessageToContentScript(message, callback)
 		});
 	});
 }
-
-// 向content-script注入JS片段
-function executeScriptToCurrentTab(code)
-{
-	getCurrentTabId((tabId) =>
-	{
-		chrome.tabs.executeScript(tabId, {code: code});
-	});
-}
-
-
-// 演示2种方式操作DOM
-
-// 修改背景色
-$('#update_bg_color').click(() => {
-	executeScriptToCurrentTab('document.body.style.backgroundColor="red";')
-});
-
-// 修改字体大小
-$('#update_font_size').click(() => {
-	sendMessageToContentScript({cmd:'update_font_size', size: 42}, function(response){});
-});
-
-// 显示badge
-$('#show_badge').click(() => {
-	chrome.browserAction.setBadgeText({text: 'New'});
-	chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]});
-});
-
-// 隐藏badge
-$('#hide_badge').click(() => {
-	chrome.browserAction.setBadgeText({text: ''});
-	chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 0, 0]});
-});
-
-// 显示桌面通知
-$('#show_notification').click(e => {
-	chrome.notifications.create(null, {
-		type: 'image',
-		iconUrl: 'img/icon.png',
-		title: '祝福',
-		message: '骚年，祝你圣诞快乐！Merry christmas!',
-		imageUrl: 'img/sds.png'
-	});
-});
-
-$('#check_media').click(e => {
-	alert('即将打开一个有视频的网站，届时将自动检测是否存在视频！');
-	chrome.tabs.create({url: 'http://www.w3school.com.cn/tiy/t.asp?f=html5_video'});
-});
